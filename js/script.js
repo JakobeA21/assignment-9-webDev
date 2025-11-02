6c3667043ce1aac035f29ed83d902ff24550f685
JakobeA21 <111141253+JakobeA21@users.noreply.github.com>
2025-11-02 16:24:15 -0600

    Add API endpoint for Giphy GIF search

--- js/script.js
+++ js/script.js
@@ -1,1 +1,2 @@
 console.log("script.js loaded");
+const endpoint = 'https://api.giphy.com/v1/gifs/search?api_key=CZ2pUyYBDn0FgziImpa57p10bXLDWHGr&q=dogs&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips'
